400e78ac1c86c5150867331b4d00293734325fbe
satyam4853 <vaishnavsatyam48532gmail.com>
2021-08-31 15:47:22 +0530

    [satyam] feat: Welcome Message

---
+++ User-Registeration-Using-Lambda/User-Registeration-Using-Lambda/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace User_Registeration_Using_Lambda
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("**************Welcome to User Registeration using Lambda Problem***************");
+        }
+    }
+}
