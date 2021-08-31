c669897428b49a7ddf2cc4063ddb718a1e4331a3
satyam4853 <vaishnavsatyam48532gmail.com>
2021-08-31 16:02:23 +0530

    [satyam] feat: Write JUnit

---
+++ User-Registeration-Using-Lambda/TestProject1/UnitTest1.cs
@@ -0,0 +1,138 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using User_Registeration_Using_Lambda;
+
+namespace TestProject1
+{
+    [TestClass]
+
+    public class UnitTest1
+    {
+        public User user;
+
+        [TestInitialize]
+        public void SetUp()
+        {
+            user = new User();
+        }
+        /// <summary>
+        /// Happy TestCases
+        /// </summary>
+        [TestMethod]
+        public void FirstNameValidationShouldReturnTrue()
+        {
+            //Arrange
+            string firstName = "Satyam";
+            //Act
+            bool result = user.ValidateFirstName(firstName);
+            //Assert
+            Assert.IsTrue(result);
+        }
+        [TestMethod]
+        public void LastNameValidationShouldReturnTrue()
+        {
+            //Arrange
+            string lastName = "Vaishnav";
+            //Act
+            bool result = user.ValidateLastName(lastName);
+            //Assert
+            Assert.IsTrue(result);
+        }
+        [TestMethod]
+        public void EmailValidationShouldReturnTrue()
+        {
+            //Arrange
+            string email = "satya@gmail.com";
+            //Act
+            bool result = user.ValidateEmail(email);
+            //Assert
+            Assert.IsTrue(result);
+        }
+        [TestMethod]
+        public void MobileNumberValidationShouldReturnTrue()
+        {
+            //Arrange
+            string mobileNumber = "91 8446019597";
+            //Act
+            bool result = user.ValidateMobileNumber(mobileNumber);
+            //Assert
+            Assert.IsTrue(result);
+        }
+        [TestMethod]
+        public void PasswordValidationShouldReturnTrue()
+        {
+            //Arrange
+            string password = "satya@4853";
+            //Act
+            bool result = user.ValidatePassword(password);
+            //Assert
+            Assert.IsTrue(result);
+        }
+        /// <summary>
+        /// Sad Test Cases
+        /// </summary>
+        [TestMethod]
+        public void FirstNameValidationShouldReturnFalse()
+        {
+            //Arrange
+            string firstName = "Rushikesh";
+            //Act
+            bool result = user.ValidateFirstName(firstName);
+            //Assert
+            Assert.IsTrue(result);
+        }
+        [TestMethod]
+        public void LastNameValidationShouldReturnFalse()
+        {
+            //Arrange
+            string lastName = "Badhe";
+            //Act
+            bool result = user.ValidateLastName(lastName);
+            //Assert
+            Assert.IsTrue(result);
+        }
+        [TestMethod]
+        public void EmailValidationShouldReturnFalse()
+        {
+            //Arrange
+            string email = "Rushi@gamil.com";
+            //Act
+            bool result = user.ValidateEmail(email);
+            //Assert
+            Assert.IsTrue(result);
+        }
+        [TestMethod]
+        public void MobileNumberValidationShouldReturnFalse()
+        {
+            //Arrange
+            string mobileNumber = "91 9011519390";
+            //Act
+            bool result = user.ValidateMobileNumber(mobileNumber);
+            //Assert
+            Assert.IsTrue(result);
+        }
+        [TestMethod]
+        public void PasswordValidationShouldReturnFalse()
+        {
+            //Arrange
+            string password = "rushi456";
+            //Act
+            bool result = user.ValidatePassword(password);
+            //Assert
+            Assert.IsTrue(result);
+        }
+        [TestMethod]
+        [DataRow("abc@yahoo.com")]
+        [DataRow("abc-100@yahoo.com")]
+        [DataRow("abc.100@yahoo.com")]
+        [DataRow("abc111@abc.com")]
+        [DataRow("abc-100@abc.net")]
+        [DataRow("abc.100@abc.com.au")]
+        [DataRow("abc@1.com")]
+        [DataRow("abc@gmail.com.com")]
+        [DataRow("abc+100@gmail.com")]
+        public void ValidateSampleEmailsShouldReturnTrue(string email)
+        {
+            Assert.IsTrue(user.ValidateEmail(email));
+        }
+    }
+}
--- User-Registeration-Using-Lambda/User-Registeration-Using-Lambda/Program.cs
+++ User-Registeration-Using-Lambda/User-Registeration-Using-Lambda/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace User_Registeration_Using_Lambda
 {
@@ -7,6 +8,101 @@ namespace User_Registeration_Using_Lambda
         static void Main(string[] args)
         {
             Console.WriteLine("**************Welcome to User Registeration using Lambda Problem***************");
+            bool flag = true;
+            while (flag)
+            {
+                Console.WriteLine("1.First Name Validation");
+                Console.WriteLine("2.Last Name Validation");
+                Console.WriteLine("3.Email Validation");
+                Console.WriteLine("4.Mobile Number Validation");
+                Console.WriteLine("5.Password Validation");
+                Console.WriteLine("6.Validating Sample emails");
+                Console.WriteLine("0.Exit");
+                Console.WriteLine("Choose an option : ");
+                int option = Convert.ToInt32(Console.ReadLine());
+                User user = new User();
+                bool result;
+                switch (option)
+                {
+                    case 1:
+                        Console.WriteLine("Enter first name : ");
+                        string firstName = Console.ReadLine();
+                        result = user.ValidateFirstName(firstName);
+                        if (result)
+                            Console.WriteLine("It is valid");
+                        else
+                            Console.WriteLine("It is not valid");
+                        break;
+                    case 2:
+                        Console.WriteLine("Enter last name : ");
+                        string lastName = Console.ReadLine();
+                        result = user.ValidateLastName(lastName);
+                        if (result)
+                            Console.WriteLine("It is valid");
+                        else
+                            Console.WriteLine("It is not valid");
+                        break;
+                    case 3:
+                        Console.WriteLine("Enter email");
+                        string email = Console.ReadLine();
+                        result = user.ValidateEmail(email);
+                        if (result)
+                            Console.WriteLine("It is valid");
+                        else
+                            Console.WriteLine("It is not valid");
+                        break;
+                    case 4:
+                        Console.WriteLine("Enter a Mobile Number to validate : ");
+                        string mobileNumber = Console.ReadLine();
+                        result = user.ValidateMobileNumber(mobileNumber);
+                        if (result)
+                            Console.WriteLine("It is valid");
+                        else
+                            Console.WriteLine("It is not valid");
+                        break;
+                    case 5:
+                        Console.WriteLine("Enter a Password to validate : ");
+                        string password = Console.ReadLine();
+                        result = user.ValidatePassword(password);
+                        if (result)
+                            Console.WriteLine("It is valid");
+                        else
+                            Console.WriteLine("It is not valid");
+                        break;
+                    case 6:
+                        // Validating Sample Emails............
+                        List<string> sampleEmailList = new List<string>()
+                        {
+                            "abc@yahoo.com",
+                            "abc-100@yahoo.com",
+                            "abc.100@yahoo.com",
+                            "abc111@abc.com",
+                            "abc-100@abc.net",
+                            "abc.100@abc.com.au",
+                            "abc@1.com",
+                            "abc@gmail.com.com",
+                            "abc+100@gmail.com"
+                        };
+                        foreach (string emails in sampleEmailList)
+                        {
+                            if (user.ValidateEmail(emails))
+                            {
+                                Console.WriteLine(" [ " + emails + " ]\t is Valid Email");
+                            }
+                            else
+                            {
+                                Console.WriteLine(" [ " + emails + " ]\t is Invalid Email");
+                            }
+                        }
+                        break;
+                    case 0:
+                        flag = false;
+                        break;
+                    default:
+                        break;
+                }
+            }
         }
     }
 }
+        
